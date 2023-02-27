0360405e114b42baf69c99418ca2195f0f4e3e54
Rafael <94694500+RafaelLucasSegalRamos@users.noreply.github.com>
2023-02-27 11:53:15 -0300

    nova_navigation_bar

---
+++ Vertical_menu_tab_design/JS/Tab.js
@@ -0,0 +1,23 @@
+const list = document.querySelectorAll('li')
+
+for (let i = 0; i < list.length; i++)
+{
+    list[i].onmouseover = function()
+    {
+        let j = 0
+        while (j < list.length){
+            list[j++].className = 'list'
+        }
+        list[i].className = 'list active'   
+    }
+}
+
+list.forEach(elements => {
+    elements.addEventListener('mouseenter',function(event)
+    {
+        let bg = document.querySelector('body')
+        let color = event.target.getAttribute('data-color')
+        bg.style.background = color
+    }) 
+})
+
