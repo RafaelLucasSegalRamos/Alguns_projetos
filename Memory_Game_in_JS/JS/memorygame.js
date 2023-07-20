cf6578aa6db879feb97a77aa227bd51ec4c6c5ed
Rafael <94694500+RafaelLucasSegalRamos@users.noreply.github.com>
2023-07-20 13:15:31 -0300

    End of teh memory game project

--- Memory_Game_in_JS/JS/memorygame.js
+++ Memory_Game_in_JS/JS/memorygame.js
@@ -8,8 +8,37 @@ for (var i = 0; i<emojis.length; i++)
     let card = document.createElement("div")
     card.className = "card"
     card.innerHTML = shuffle_emojis[i]
-    card.draggable = false
+    
     document.getElementById("game").appendChild(card)
+    card.setAttribute('draggable', 'false')
 
+    card.onclick = function() {
+        card.classList.add("flip")
+
+        setTimeout(function() {
+            if (document.querySelectorAll(".flip").length>1)
+            {
+                if (document.querySelectorAll(".flip")[0].innerHTML === document.querySelectorAll(".flip")[1].innerHTML)
+                {
+                    document.querySelectorAll(".flip")[0].classList.add("done")
+                    document.querySelectorAll(".flip")[1].classList.add("done")
+                    document.querySelectorAll(".flip")[0].classList.remove("flip")
+                    document.querySelectorAll(".flip")[0].classList.remove("flip")
+
+                    if (document.querySelectorAll(".done").length == emojis.length)
+                    {
+                        document.getElementById("msgfinal").classList.add("fim")
+                        document.getElementById("msgfinal").innerHTML = " You Won! "
+                    }
+                }
+                else
+                {
+                    document.querySelectorAll(".flip")[0].classList.remove("flip")
+                    document.querySelectorAll(".flip")[0].classList.remove("flip")
+
+                }
+            }
+        }, 1000)
+    }
 }
 
