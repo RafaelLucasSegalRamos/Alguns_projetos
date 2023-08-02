b9d58cee4fe5581a50ad1a641465e3474404962c
Rafael <94694500+RafaelLucasSegalRamos@users.noreply.github.com>
2023-08-02 14:54:25 -0300

    end of side_bar_menu

--- Side_Bar_Menu/JS/dark_mode.js
+++ Side_Bar_Menu/JS/dark_mode.js
@@ -3,4 +3,10 @@ var body = document.querySelector("body");
 
 botao.addEventListener("click", function(){
     body.classList.toggle("dark");
+    if(body.classList.contains("dark")){
+    botao.innerHTML = '<span class="iconBox"><i class="fa-solid fa-sun"></i></span> <span class="textBox">Light Mode</span>'
+    }
+    else{
+    botao.innerHTML = '<span class="iconBox"><i class="fa-solid fa-moon"></i></span> <span class="textBox">Dark Mode</span>'
+    }
 })
--- Side_Bar_Menu/JS/estica_aba.js
+++ Side_Bar_Menu/JS/estica_aba.js
@@ -1,6 +1,13 @@
 var botao = document.getElementById("button-retreat");
 var menu = document.querySelector(".float-side-bar");
+let botao2 = document.querySelector("#pesquisa .pes");
 
 botao.addEventListener("click", function(){
     menu.classList.toggle("estica");
+    botao2.classList.toggle("pes");
+});
+
+botao2.addEventListener("click", function(){
+    menu.classList.toggle("estica");
+    botao2.classList.toggle("pes");
 });
