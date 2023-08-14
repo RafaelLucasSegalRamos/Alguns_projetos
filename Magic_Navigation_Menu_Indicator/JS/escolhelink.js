2439faa7e103d44598f4a7320077570bf59df478
Rafael <94694500+RafaelLucasSegalRamos@users.noreply.github.com>
2023-08-14 16:50:21 -0300

    terminar amanha

---
+++ Magic_Navigation_Menu_Indicator/JS/escolhelink.js
@@ -0,0 +1,24 @@
+var marker = document.getElementById('indicator');
+var nav = document.querySelector('nav');
+let items = document.querySelectorAll('nav a');
+
+nav.addEventListener('click', (e) => {
+    marker.classList.toggle('change');
+})
+
+function indicator(e) {
+    marker.style.left = e.offsetLeft + 'px';
+    marker.style.width = e.offsetWidth + 'px';
+    marker.style.display = 'block';
+    marker.style.filter = `hue-rotate(${Math.random() * 360}deg)`;
+}
+
+items.forEach(link => {
+    link.addEventListener('click', (e) => {
+        indicator(e.target);
+    })
+})
+
+function addActiveClass(){
+    
+}
