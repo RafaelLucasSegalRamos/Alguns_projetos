d2bf5c0ac0f9f7f1347a73fb239f0ac4bcb649e7
Rafael <rafaelsegalramos@gmail.com>
2023-09-01 12:26:05 -0300

    end of this project

--- Reveal_Elements_On_Scroll/JS/reveal.js
+++ Reveal_Elements_On_Scroll/JS/reveal.js
@@ -2,8 +2,29 @@ for (let i = 1;i<=60;i++)
 {
     let box = document.createElement('div')
     box.classList.add('box')
+    box.style.background = randomColors()
     document.querySelector('.container').appendChild(box)
 }
+
+//now is just adding colors to the boxes
+
+let rdnColors = document.querySelectorAll('.box')
+function randomColors()
+{
+    let chars = '0123456789ABCDEF'
+    const colorLength = 6
+    var color = ''
+
+    for(let i = 0;i<colorLength;i++)
+    {
+        ranCor = Math.floor(Math.random() * chars.length)
+        color += chars.substring(ranCor,ranCor+1)
+    }
+    return "#"+color
+}
+
+randomColors()
+// now we go back doing the scroll trigger
 
 let boxes = document.querySelectorAll('.box')
 
@@ -11,7 +32,7 @@ function scrollTrigger()
 {
     boxes.forEach(box => {
         
-        if(box.offsetTop < window.scrollY)
+        if(box.offsetTop+100 < window.scrollY)
         {
             box.classList.add('show')
         }
