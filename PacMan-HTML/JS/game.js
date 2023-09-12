caabaf5308a2a65cd8a0632e1108d90ef6646d11
Rafael <rafaelsegalramos@gmail.com>
2023-09-12 16:52:59 -0300

    Two projects, 1 yet to finish, and the other is complet

---
+++ PacMan-HTML/JS/game.js
@@ -0,0 +1,79 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+const pacFrames = document.getElementById('animation');
+const ghostsFrames = document.getElementById('ghosts');
+
+let craeteRect = (x, y, width, height, color) => {
+
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, width, height);
+}
+let fps = 30
+let oneBlockSize = 20
+let wallSpaceWidth = oneBlockSize / 1.1
+let wallOffSet = oneBlockSize - wallSpaceWidth
+
+let map = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], // 0
+    [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1], // 1
+    [1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1], // 2
+    [1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1], // 3
+    [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1], // 4
+    [1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1], // 5
+    [1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1], // 6
+    [1, 1, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 1, 1, 1], // 7
+    [0, 0, 0, 0, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 0, 0, 0, 0], // 8
+    [1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 0, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1], // 9
+    [2, 2, 2, 2, 2, 2, 2, 2, 1, 0, 0, 0, 1, 2, 2, 2, 2, 2, 2, 2, 2], // 10
+    [1, 1, 1, 1, 1, 2, 1, 2, 1, 0, 0, 0, 1, 2, 1, 2, 1, 1, 1, 1, 1], // 11
+    [0, 0, 0, 0, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 0, 0, 0, 0], // 12
+    [0, 0, 0, 0, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 0, 0, 0, 0], // 13
+    [1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 1], // 14
+    [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1], // 15
+    [1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1], // 16
+    [1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1], // 17
+    [1, 1, 2, 2, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 2, 2, 1, 1], // 18
+    [1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1], // 19
+    [1, 2, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 2, 1], // 20
+    [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1], // 21
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], // 22
+    
+];
+
+let gameLoop = () => {
+    update()
+    draw()
+}
+
+let update = () => {
+
+}
+
+let draw = () => {
+    craeteRect(0, 0 , canvas.width, canvas.height, 'black')
+    drawWalls()
+}
+
+let gameInterval = setInterval(gameLoop, 1000 / fps);
+
+let drawWalls = () => {
+    for (let i = 0; i < map.length; i++) {
+        for (let j = 0; j < map[0].length; j++) {
+            if (map[i][j] == 1) {// then it is a Wall
+                craeteRect(j * oneBlockSize, i * oneBlockSize, oneBlockSize, oneBlockSize, '#342DCa');
+                if(j > 0 && map[i][j-1] == 1)
+                {
+                    craeteRect(j*oneBlockSize, i*oneBlockSize, oneBlockSize, oneBlockSize, '#342DCa')
+                }
+            }
+
+        }
+        // for (let j = 0; j < map[0].length; j++) {
+        //     if (map[i][j] == 2) { // then is a dot to collect
+        //         craeteRect(j * oneBlockSize, i * oneBlockSize, oneBlockSize/2, oneBlockSize/2, 'red');
+        //     }
+
+        // }
+    }
+}
