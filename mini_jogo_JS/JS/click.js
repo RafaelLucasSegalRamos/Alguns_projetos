0863103f224e627d4e47c7cd8dc0f9fe3f375fe5
Rafael <rafaelsegalramos@gmail.com>
2023-12-28 15:39:46 -0300

    update

--- mini_jogo_JS/JS/click.js
+++ mini_jogo_JS/JS/click.js
@@ -1,60 +1,48 @@
 beancan = document.querySelector("#bean-can-image");
 beans = document.getElementById("beans");
-cost = document.querySelector(".click-cost");
 buyButton = document.querySelectorAll(".upgrade");
-ClLevel = document.querySelector(".click-level");
 Cincrease = document.querySelector(".cursor_increase");
 beanPerClick = document.getElementById("bpc");
 
 
 var bpc = 0.1
+cont = 0
 
 beancan.addEventListener("click", function() {
     resp = (parseFloat(beans.innerHTML) + parseFloat(bpc)).toFixed(1);
     beans.innerHTML = resp;
 });
 
-// buyButton[0].addEventListener("click", function() {
-//     if (parseFloat(beans.innerHTML) >= parseFloat(cost.innerHTML)) {
-//         beans.innerHTML = (parseFloat(beans.innerHTML) - parseFloat(cost.innerHTML)).toFixed(1);
-//         cost.innerHTML = (parseFloat(cost.innerHTML) * 1.2).toFixed(1);
-//         console.log(buyButton);
-//         if (bpc < 5) {
-//             bpc = (bpc * 2).toFixed(1);
-//             Cincrease.innerHTML = bpc;
-//             beanPerClick.innerHTML = bpc;
-//             cost.innerHTML = (parseFloat(cost.innerHTML) * 1.2).toFixed(1);
-//         }
-//         else {
-//             bpc = (bpc * 1.2).toFixed(1);
-//             Cincrease.innerHTML = (bpc * 0.5).toFixed(1);
-//             beanPerClick.innerHTML = bpc;
-//             cost.innerHTML = (parseFloat(cost.innerHTML) * 1.3).toFixed(1);
-//         }
-//         ClLevel.innerHTML = parseInt(ClLevel.innerHTML) + 1;
-//     }
-// });
+for (let i = 0; i < buyButton.length; i++) {
+    
+    buyButton[i].addEventListener("click", function() {
+        let Level = document.querySelectorAll(`.level`)[i]
+        let cost = document.querySelectorAll(`.cost`)[i]
+        let increase = document.querySelectorAll(`.increase`)[i]
 
-buyButton.forEach(element => {
-    element.addEventListener("click", function() {
+        
         if (parseFloat(beans.innerHTML) >= parseFloat(cost.innerHTML)) {
-            
+            // Não precisa mexer
             beans.innerHTML = (parseFloat(beans.innerHTML) - parseFloat(cost.innerHTML)).toFixed(1);
-            cost.innerHTML = (parseFloat(cost.innerHTML) * 1.2).toFixed(1);
-            console.log(buyButton);
-            if (bpc < 5) {
-                bpc = (bpc * 2).toFixed(1);
-                Cincrease.innerHTML = bpc;
-                beanPerClick.innerHTML = bpc;
-                cost.innerHTML = (parseFloat(cost.innerHTML) * 1.2).toFixed(1);
+            cost.innerHTML = (parseFloat(cost.innerHTML) * 1.5).toFixed(1);
+            // Aqui precisa
+            if (parseFloat(Level.innerHTML) < 15) {
+                Level.innerHTML = (parseInt(Level.innerHTML) + 1).toFixed(0);
+                bpc = bpc + parseFloat(increase.innerHTML);	
+                increase.innerHTML = (parseFloat(increase.innerHTML) * 1.5).toFixed(1)
+                beanPerClick.innerHTML = bpc.toFixed(1);
+                cost.innerHTML = (parseFloat(cost.innerHTML) * 1.1).toFixed(1);
             }
             else {
-                bpc = (bpc * 1.2).toFixed(1);
-                Cincrease.innerHTML = (bpc * 0.5).toFixed(1);
-                beanPerClick.innerHTML = bpc;
+                bpc = parseFloat(increase.innerHTML) + bpc;
+                Level.innerHTML = (parseInt(Level.innerHTML) + 1).toFixed(0);
+                increase.innerHTML = (parseFloat(increase.innerHTML) * 1.3).toFixed(1);
+                beanPerClick.innerHTML = bpc.toFixed(1);
                 cost.innerHTML = (parseFloat(cost.innerHTML) * 1.3).toFixed(1);
             }
-            ClLevel.innerHTML = parseInt(ClLevel.innerHTML) + 1;
         }
-    });
-});
+        
+    })
+    
+    
+}
