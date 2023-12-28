c0416dc292b405e36e17b7c2fda99dd617010e3d
Rafael <rafaelsegalramos@gmail.com>
2023-12-28 16:39:48 -0300

    update

--- mini_jogo_JS/JS/click.js
+++ mini_jogo_JS/JS/click.js
@@ -3,9 +3,11 @@ beans = document.getElementById("beans");
 buyButton = document.querySelectorAll(".upgrade");
 Cincrease = document.querySelector(".cursor_increase");
 beanPerClick = document.getElementById("bpc");
+beanPerSecond = document.getElementById("bps");
 
 
-var bpc = 0.1
+var bpc = parseFloat(beanPerClick.innerHTML);
+var bps = parseFloat(beanPerSecond.innerHTML);
 cont = 0
 
 beancan.addEventListener("click", function() {
@@ -14,35 +16,61 @@ beancan.addEventListener("click", function() {
 });
 
 for (let i = 0; i < buyButton.length; i++) {
-    
-    buyButton[i].addEventListener("click", function() {
-        let Level = document.querySelectorAll(`.level`)[i]
-        let cost = document.querySelectorAll(`.cost`)[i]
-        let increase = document.querySelectorAll(`.increase`)[i]
 
-        
-        if (parseFloat(beans.innerHTML) >= parseFloat(cost.innerHTML)) {
-            // Não precisa mexer
-            beans.innerHTML = (parseFloat(beans.innerHTML) - parseFloat(cost.innerHTML)).toFixed(1);
-            cost.innerHTML = (parseFloat(cost.innerHTML) * 1.5).toFixed(1);
-            // Aqui precisa
-            if (parseFloat(Level.innerHTML) < 15) {
-                Level.innerHTML = (parseInt(Level.innerHTML) + 1).toFixed(0);
-                bpc = bpc + parseFloat(increase.innerHTML);	
-                increase.innerHTML = (parseFloat(increase.innerHTML) * 1.5).toFixed(1)
-                beanPerClick.innerHTML = bpc.toFixed(1);
-                cost.innerHTML = (parseFloat(cost.innerHTML) * 1.1).toFixed(1);
-            }
-            else {
-                bpc = parseFloat(increase.innerHTML) + bpc;
-                Level.innerHTML = (parseInt(Level.innerHTML) + 1).toFixed(0);
-                increase.innerHTML = (parseFloat(increase.innerHTML) * 1.3).toFixed(1);
-                beanPerClick.innerHTML = bpc.toFixed(1);
-                cost.innerHTML = (parseFloat(cost.innerHTML) * 1.3).toFixed(1);
-            }
-        }
-        
-    })
+    if (i == 0) {
+        buyButton[i].addEventListener("click", function() {
+            let Level = document.querySelectorAll(`.level`)[i]
+            let cost = document.querySelectorAll(`.cost`)[i]
+            let increase = document.querySelectorAll(`.increase`)[i]
     
+            
+            if (parseFloat(beans.innerHTML) >= parseFloat(cost.innerHTML)) {
+                beans.innerHTML = (parseFloat(beans.innerHTML) - parseFloat(cost.innerHTML)).toFixed(1);
+                cost.innerHTML = (parseFloat(cost.innerHTML) * 1.5).toFixed(1);
+                if (parseFloat(Level.innerHTML) < 15) {
+                    Level.innerHTML = (parseInt(Level.innerHTML) + 1).toFixed(0);
+                    bpc = bpc + parseFloat(increase.innerHTML);	
+                    increase.innerHTML = (parseFloat(increase.innerHTML) * 1.6).toFixed(1)
+                    beanPerClick.innerHTML = bpc.toFixed(1);
+                    cost.innerHTML = (parseFloat(cost.innerHTML) * 1.1).toFixed(1);
+                }
+                else {
+                    bpc = parseFloat(increase.innerHTML) + bpc;
+                    Level.innerHTML = (parseInt(Level.innerHTML) + 1).toFixed(0);
+                    increase.innerHTML = (parseFloat(increase.innerHTML) * 1.3).toFixed(1);
+                    beanPerClick.innerHTML = bpc.toFixed(1);
+                    cost.innerHTML = (parseFloat(cost.innerHTML) * 1.4).toFixed(1);
+                }
+            }
+            
+        })
+    }
+    else{
+        buyButton[i].addEventListener("click", function() {
+            let Level = document.querySelectorAll(`.level`)[i]
+            let cost = document.querySelectorAll(`.cost`)[i]
+            let increase = document.querySelectorAll(`.increase`)[i]
     
-}
+            
+            if (parseFloat(beans.innerHTML) >= parseFloat(cost.innerHTML)) {
+                beans.innerHTML = (parseFloat(beans.innerHTML) - parseFloat(cost.innerHTML)).toFixed(1);
+                if (parseFloat(Level.innerHTML) < 15) {
+                    Level.innerHTML = (parseInt(Level.innerHTML) + 1).toFixed(0);
+                    bps = bps + parseFloat(increase.innerHTML);	
+                    increase.innerHTML = (parseFloat(increase.innerHTML) * 1.4).toFixed(1)
+                    beanPerSecond.innerHTML = bps.toFixed(1);
+                    cost.innerHTML = (parseFloat(cost.innerHTML) * 1.1).toFixed(1);
+                }
+                else {
+                    bps = parseFloat(increase.innerHTML) + bps;
+                    Level.innerHTML = (parseInt(Level.innerHTML) + 1).toFixed(0);
+                    increase.innerHTML = (parseFloat(increase.innerHTML) * 1.3).toFixed(1);
+                    beanPerSecond.innerHTML = bps.toFixed(1);
+                    cost.innerHTML = (parseFloat(cost.innerHTML) * 1.6).toFixed(1);
+                }
+            }})
+            
+            setInterval(function() {
+                beans.innerHTML = (parseFloat(beans.innerHTML) + parseFloat(bps/2)).toFixed(1);
+            }, 1000);
+    }}
